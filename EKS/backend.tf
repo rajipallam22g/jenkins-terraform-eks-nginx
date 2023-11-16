7bcbbb9f1fe93d109fdd3c53e6ac59d421aca3b7
rajipallam22g <113780580+rajipallam22g@users.noreply.github.com>
2023-11-16 20:42:25 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "test-cicd-tf-eks"
+    bucket         = "sayantan-cicd-tf-eks-cicd-tf-eks"
     key            = "eks/terraform.tfstate"
     region         = "ap-southeast-1"
     encrypt        = true
