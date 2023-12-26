7fa508ca32cbd7ee9f6b16f871298af4c312407a
rajipallam22g <113780580+rajipallam22g@users.noreply.github.com>
2023-12-26 23:03:15 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "sayantan-cicd-tf-eks"
     key            = "eks/terraform.tfstate"
-    region         = "ap-southeast-1"
+    region         = "ap-southeast-2"
     encrypt        = true
     dynamodb_table = "eks-terraform-lock"
   }
